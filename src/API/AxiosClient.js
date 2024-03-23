16e4bd973d6962ab3e0ed68eab69add41ea29f4e
truong <quangtruongpham08032003@gmail.com>
2024-03-23 10:01:42 +0700

    modified AxiosClient.js in API

--- src/API/AxiosClient.js
+++ src/API/AxiosClient.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
+/*
+Thiết lập URL API, 
+set thời gian chờ 5p cho yêu cầu thực hiện phiên bản,
+sau 5p được coi là lỗi
+*/
 const instance = axios.create(
     {
         baseURL: process.env.REACT_APP_URL_API,
         timeout: 300000
     }
 );
+/* 
+Hàm xử lý phản hồi
+*/
 instance.interceptors.response.use(
     (response) =>{
         return response.data
